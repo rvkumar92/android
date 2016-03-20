2a0041361aaf81b5e5673520b756b37504ecc330
Vijay Kumar <ind.vijayworldz@gmail.com>
2016-03-20 23:36:56 +0530

    login page that redirects to another activity using intent

---
+++ Login/app/src/main/java/com/rvkumar/login/LoginPage.java
@@ -0,0 +1,34 @@
+package com.rvkumar.login;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class LoginPage extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+        Intent intent = getIntent();
+        String userName = intent.getStringExtra("userName");
+
+        TextView loginView = (TextView) findViewById(R.id.loginview);
+        loginView.setTextSize(25);
+        loginView.setText("Welcome !" + userName);
+        loginView.setTextColor(Color.GREEN);
+        Button back = (Button) findViewById(R.id.back);
+        //back.setText("Back");
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.activity_main);
+            }
+        });
+
+    }
+}
--- Login/app/src/main/java/com/rvkumar/login/MainActivity.java
+++ Login/app/src/main/java/com/rvkumar/login/MainActivity.java
@@ -1,13 +1,104 @@
 package com.rvkumar.login;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    final String uName= "ind.vijayworldz@gmail.com";
+    final String pwd = "born2win";
 
+    boolean userStatus = false;
+    boolean passwordStatus = false;
+    Button login, clear;
+    EditText username, password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Intent intent = new Intent(this,LoginPage.class);
+        login = (Button) findViewById(R.id.login);
+        clear = (Button) findViewById(R.id.clear);
+        username = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
+        login.setEnabled(false);
+        login.setOnClickListener(new Button.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                intent.putExtra("userName", username.getText().toString());
+                intent.addFlags(intent.FLAG_ACTIVITY_CLEAR_TASK);
+                intent.addFlags(intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+            }
+        });
+
+        clear.setOnClickListener(new Button.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                username.setText("");
+                password.setText("");
+            }
+        });
+
+        username.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                System.out.println(username.getText().toString());
+                System.out.println(uName);
+                System.out.println(uName.equals(username.getText().toString()));
+                if (uName.equals(username.getText().toString())) {
+                    userStatus = true;
+                } else {
+                    userStatus = false;
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (userStatus && passwordStatus) {
+                    login.setEnabled(true);
+                } else {
+                    login.setEnabled(false);
+                }
+            }
+        });
+
+        password.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(pwd.equals(password.getText().toString())){
+                    passwordStatus = true;
+                }else{
+                    passwordStatus = false;
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (userStatus && passwordStatus) {
+                    login.setEnabled(true);
+                } else {
+                    login.setEnabled(false);
+                }
+            }
+        });
     }
 }
